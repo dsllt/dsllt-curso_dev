f3c23fed5cf6e160c53f27c1fd511c0528cd0a98
Dayane Sallet <dayanesallet@hotmail.com>
2023-04-09 13:26:37 +0000

    Modifica a página principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-  return <h1>Teste</h1>
+  return <h1>You must do things that remind you who you are and who you are not. You must dance between motion and stillness. </h1>
 }
 
 export default Home;
