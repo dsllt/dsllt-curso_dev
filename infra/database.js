7e8f02ba86f07e45865df364182e034459cfc1ac
dsllt <dayanesallet@hotmail.com>
2023-12-20 13:55:10 -0300

    makes database.js more robust using error handling

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,23 @@
-import { Client } from 'pg'
+import { Client } from "pg";
 
 async function query(queryObject) {
+  let result;
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
-  })
-  await client.connect()
-  const result = await client.query(queryObject)
-  await client.end()
-
-  return client
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  try {
+    await client.connect();
+    result = await client.query(queryObject);
+    return result;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
