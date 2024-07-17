5187f673f51316900cb519b3709ae4c43e771487
dsllt <dayanesallet@hotmail.com>
2024-07-16 21:14:01 -0300

    fix prettier style linting

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -10,9 +10,9 @@ import database from "infra/database";
 
 export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
-  if(!allowedMethods.includes(request.method)){
+  if (!allowedMethods.includes(request.method)) {
     return response.status(405).json({
-      error: `Method "${request.method}" not allowed`
+      error: `Method "${request.method}" not allowed`,
     });
   }
 
@@ -27,18 +27,18 @@ export default async function migrations(request, response) {
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
-    }
+    };
     if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationOptions);   
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       return response.status(200).json(pendingMigrations);
     }
-  
+
     if (request.method === "POST") {
       const migratedMigrations = await migrationRunner({
-        ...defaultMigrationOptions, 
-        dryRun: false
+        ...defaultMigrationOptions,
+        dryRun: false,
       });
-  
+
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
@@ -47,11 +47,6 @@ export default async function migrations(request, response) {
   } catch (err) {
     throw err;
   } finally {
-    dbClient.end()
+    dbClient.end();
   }
-  
-
-
-
-
 }
