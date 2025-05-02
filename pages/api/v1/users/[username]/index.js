d635d787f0600f47cef78b55823b6d1a0e6aae97
dsllt <dayanesallet@hotmail.com>
2025-05-02 18:20:33 -0300

    feat: add support for patch on /api/v1/users/[usersame]

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -10,6 +10,7 @@ import user from "models/user";
 
 const router = createRouter();
 router.get(getHandler);
+router.patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -17,4 +18,11 @@ async function getHandler(request, response) {
   const username = request.query.username;
   const userFound = await user.findOneByUsername(username);
   return response.status(200).json(userFound);
+}
+
+async function patchHandler(request, response) {
+  const username = request.query.username;
+  const userInputValues = request.body;
+  const updatedUser = await user.update(username, userInputValues);
+  return response.status(200).json(updatedUser);
 }
