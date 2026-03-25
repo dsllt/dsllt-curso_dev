7ede1fc741f94431d046885ae5265d4c9133345a
dsllt <dayanesallet@hotmail.com>
2026-03-25 10:53:44 -0300

    refactor: add default user coverage for GET /api/v1/status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -51,4 +51,29 @@ describe("GET to /api/v1/status", () => {
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
   });
+  describe("Default user", () => {
+    test("Retrieving current system status", async () => {
+      const defaultUser = await orchestrator.createUser();
+      await orchestrator.activateUser(defaultUser);
+      const sessionObjectDefaultUser =
+        await orchestrator.createSession(defaultUser);
+
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        method: "GET",
+        headers: {
+          Cookie: `session_id=${sessionObjectDefaultUser.token}`,
+        },
+      });
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      console.log(responseBody);
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
