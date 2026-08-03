a163f4771d6e99e021c12898ca2d403238c1e9dc
dsllt <dayanesallet@hotmail.com>
2026-08-03 20:40:04 -0300

    feat: install and configure GitHub Primer

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import "@primer/primitives/dist/css/functional/themes/light.css";
+import { ThemeProvider, BaseStyles } from "@primer/react";
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <BaseStyles>
+        <Component {...pageProps} />
+      </BaseStyles>
+    </ThemeProvider>
+  );
+}
